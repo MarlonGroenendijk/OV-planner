692ef11df71d87aee27ccd4392a1158abb927671
Marlon Groenendijk <mail@marlon.ws>
2018-03-28 13:06:13 +0200

    Updated search
    Now includes colors.
    Made it more readable.
    Added full URL for use in other scripts.

--- search.sh
+++ search.sh
@@ -5,7 +5,8 @@ wget "https://api.9292.nl/0.1/locations/?lang=en-GB&q=$1" -O /tmp/OVsearch -o /d
 readarray -t ID < <(cat /tmp/OVsearch | jq -r '.locations[] .id')
 readarray -t name < <(cat /tmp/OVsearch | jq -r '.locations[] .name')
 
+echo "Resultaten: "
 for ix in ${!ID[*]}
 do
-    echo -e "ID: ${ID[$ix]} - Naam: ${name[$ix]}"
+    echo -e "\e[32m\e[1mNaam:\e[0m\e[39m ${name[$ix]} \e[32m\e[1mID:\e[0m\e[39m ${ID[$ix]} - \e[93m\e[1mOutput URL: https://api.9292.nl/0.1/locations/${ID[$ix]}/departure-times?lang=nl-NL\e[0m\e[39m"
 done
