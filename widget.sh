c51053098e2dec2bb0c0654b7769efe43da2eb51
Marlon Groenendijk <mail@marlon.ws>
2018-03-28 12:58:54 +0200

    Updated widget.sh
    More compact output.
    Added bus operator icons.

--- widget.sh
+++ widget.sh
@@ -12,8 +12,59 @@ readarray -t numberBus < <(cat /tmp/deptimeswdg | jq -r '.tabs[0] .departures[] 
 readarray -t timeBus < <(cat /tmp/deptimeswdg | jq -r '.tabs[0] .departures[] .time')
 readarray -t operatorBus < <(cat /tmp/deptimeswdg | jq -r '.tabs[0] .departures[] .operatorName')
 readarray -t delayedBus < <(cat /tmp/deptimeswdg | jq -r '.tabs[0] .departures[] .realtimeText')
+	if [[ ${operatorBus[$1]} = "Arriva" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "Breng" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "Syntus" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "OV Regio IJsselmond" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "Prov Zeeland" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "GVU" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "Connexxion" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "Qbuzz" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "Levelink" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "U-OV" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "EBS" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "Flixbus" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "HTM" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "HTMbuzz" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "GVB" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "RET" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
+	if [[ ${operatorBus[$1]} = "Hermes" ]]; then
+		operatorBusIcon[$1]="" 
+	fi
 
-	echo -n " N: ${numberBus[$1]} ${destBus[$1]} - V: ${timeBus[$1]}"
+	echo -n "${operatorBusIcon[$1]} ${numberBus[$1]} ${destBus[$1]} V: ${timeBus[$1]}"
 	if [ ! "${delayedBus[$1]}" == "null" ]
 	then
 		echo -ne "${delayedBus[$1]}" 
